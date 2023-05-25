5a0f659a7954c3bf6d2f1709f35623803226a2fb
mylesw27 <myleswiegel27@gmail.com>
2023-05-25 16:03:54 -0700

    add url to allowed hosts

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['10.0.0.197', 'localhost']
+ALLOWED_HOSTS = ['10.0.0.197', 'localhost', 'tournament-time.herokuapp.com']
 
 
 # Application definition
